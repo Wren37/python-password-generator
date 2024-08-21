2c5f143d9720a785ab15bc8ae82979faa6ac685c
Wren37 <65744167+Wren37@users.noreply.github.com>
2024-08-21 00:55:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import random
 import string
 
 def generatore_password(length: int = 10):
-    alphabet = string.ascii_letters + string.digits + string.punctuation
-    password = ''.join(random.choice(alphabet) for i in range(length))
+    alphabet = string.ascii_letters + string.digits + string.punctuation    #stores all characters in a variable to use
+    password = ''.join(random.choice(alphabet) for i in range(length))      #randomly chooses out of the characters ten times (default length) to create a strong password
     return password
 
-print(generatore_password())
+print(generatore_password())    #prints password to terminal
